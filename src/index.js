e82ba2db0ca9023a1e1bf770b800a680b44cda18
Nikhil Hira <nikhil@leancode.io>
2019-03-05 10:42:19 -0800

    import css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import StorePicker from './components/StorePicker';
+import './css/style.css';
 
 // mounts the app to index.html #main
 render(<StorePicker />, document.querySelector('#main'));
