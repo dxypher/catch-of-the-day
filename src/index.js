1c8659134c75f9a68eeb9af55b6001ff6718276a
Nikhil Hira <nikhil@leancode.io>
2019-03-05 10:25:00 -0800

    mount app, create StorePicker component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+
+// mounts the app to index.html #main
+render(<StorePicker />, document.querySelector('#main'));
