6c9c64b870886ec60d7d29a3b1bdc12802fc5a32
Nikhil Hira <nikhil@leancode.io>
2019-03-06 16:17:48 -0800

    convert header to a funtional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,21 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render () {
-    return (
-      <header className="top">
-        <h1>Catch
+// () instead or {} for implicit return
+// you can also destructure props... { tagline, age } and then
+// just use tagline instead of props.tagline
+const Header = (props) => (
+  <header className="top">
+    <h1>Catch
           <span className="ofThe">
-            <span className="of">Of</span>
-            <span className="the">The</span>
-          </span>
-           Day
+        <span className="of">Of</span>
+        <span className="the">The</span>
+      </span>
+      Day
           </h1>
-        <h3 className="tagline">
-          <span>{this.props.tagline}</span>
-        </h3>
-      </header>
-    )
-  }
-}
+    <h3 className="tagline">
+      <span>{props.tagline}</span>
+    </h3>
+  </header>
+);
 
 export default Header;
