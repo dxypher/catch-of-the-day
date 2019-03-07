af370035818c6b17d59e7454c38b3ccb5ec6c76f
Nikhil Hira <nikhil@leancode.io>
2019-03-07 07:01:17 -0800

    add addFishForm comp. and method for adding fish

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import AddFishForm from './AddFishForm';
 
 export default class Inventory extends React.Component {
   render() {
     return (
       <div>
-        <p>Inventory</p>
+        <h2>Inventory</h2>
+        <AddFishForm />
       </div>
     )
   }
