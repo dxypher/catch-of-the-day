94942f8f619295631ad7cd76c41ef27c3745f650
Nikhil Hira <nikhil@leancode.io>
2019-03-07 06:28:10 -0800

    add properties myInput and goToStore to StorePicker

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,11 +3,31 @@ import { getFunName } from '../helpers';
 
 
 class StorePicker extends React.Component {
+  // constructor() {
+  //   // runs React.Component first
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+
+  myInput = React.createRef();
+  // This would require the constructor method to bind this to the method
+  // goToStore(event) {
+  //   event.preventDefault();
+  //   console.log(this);
+  // }
+
+  // Alternativly you can define a property to not need constructor
+  // bound to instance
+  goToStore = (event) => {
+    event.preventDefault();
+    console.log(this);
+  }
+
   render() {
     return (
-      <form action="" className="store-selector">
+      <form action="" className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder='Store Name' defaultValue={getFunName()}/>
+        <input type="text" ref={this.myInput} required placeholder='Store Name' defaultValue={getFunName()}/>
         <button type='submit'>Visit Store</button>
       </form>
     )
