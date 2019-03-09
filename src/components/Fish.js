f86ee59baf98b2115359e3bd5d77ed2314857b68
Nikhil Hira <nikhil@leancode.io>
2019-03-09 14:55:11 -0800

    implement add to order button

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,17 +2,28 @@ import React, { Component } from 'react';
 import { formatPrice } from '../helpers';
 
 export default class Fish extends Component {
+  handleClick = () => {
+    this.props.addToOrder(this.props.index);
+  };
+
   render() {
-    const { image, name, price, desc, status } = this.props.details;
+    const { image, name,
+            price, desc,
+            status } = this.props.details;
+    const isAvailable = status === 'available';
+
     return (
-      <li className='menu-fish'>
-        <img src={image} alt={name}/>
-        <h3 className='fish-name'>{name}
-          <span className='price'>{formatPrice(price)}</span>
+      <li className="menu-fish">
+        <img src={image} alt={name} />
+        <h3 className="fish-name">
+          {name}
+          <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add To Cart</button>
+        <button disabled={!isAvailable}
+        onClick = {this.handleClick}
+        >{isAvailable ? 'Add To Cart' : 'Sold out!'}</button>
       </li>
-    )
+    );
   }
 }
