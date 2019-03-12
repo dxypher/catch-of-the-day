01792eeaa1bb8f179cdb1cdd72abb8cf5757c2ce
Nikhil Hira <nikhil@leancode.io>
2019-03-11 18:42:15 -0700

    add edit fish form

--- src/components/App.js
+++ src/components/App.js
@@ -48,6 +48,15 @@ class App extends React.Component {
     })
   };
 
+  updateFish = (key, updatedFish) => {
+    const  fishes = { ...this.state.fishes };
+    fishes[key]   = updatedFish;
+
+    this.setState({
+      fishes
+    })
+  }
+
   loadSampleFishes = () => {
     this.setState({
       fishes: sampleFishes
@@ -75,6 +84,8 @@ class App extends React.Component {
         <Inventory
           addFish          = {this.addFish}
           loadSampleFishes = {this.loadSampleFishes}
+          fishes           = {this.state.fishes}
+          updateFish       = {this.updateFish}
         />
       </div>
     );
---
+++ src/components/EditFishForm.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class EditFishForm extends Component {
+  handleChange = (event) => {
+    const updatedFish = { 
+      ...this.props.fish,
+      [event.currentTarget.name]: event.currentTarget.value
+    };
+    this.props.updateFish(this.props.index, updatedFish)
+  }
+
+  render() {
+    const { name, price, status, desc, image } = this.props.fish;
+    
+    return (
+      <div className='fish-edit'>
+        <input type='text' name='name' onChange={this.handleChange} value={name} />
+        <input type='text' name='price' onChange={this.handleChange} value={price} />
+        <select name='status' onChange={this.handleChange} value={status}>
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea name='desc' onChange={this.handleChange} value={desc} />
+        <input type='text' name='image' onChange={this.handleChange} value={image} />
+      </div>
+    )
+  }
+}
