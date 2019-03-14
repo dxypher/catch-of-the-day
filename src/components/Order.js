9656352f8d16e62f7510110fc062b88e73d8607f
Nikhil Hira <nikhil@leancode.io>
2019-03-14 16:38:19 -0700

    and transitionOptions

--- src/components/Order.js
+++ src/components/Order.js
@@ -3,15 +3,21 @@ import { formatPrice } from '../helpers';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 export default class Order extends React.Component {
   renderOrder = (key) => {
-    const fish        = this.props.fishes[key];
-    const count       = this.props.order[key];
-    const isAvailable = fish && fish.status === 'available';
+    const fish              = this.props.fishes[key];
+    const count             = this.props.order[key];
+    const isAvailable       = fish && fish.status === 'available';
+    const transitionOptions = {
+      classNames: 'order',
+      key,
+      timeout: { enter: 250, exit: 250 }
+    };
+
     // make sure fish is loaded from Firebase
     if(!fish) return null;
 
     if(!isAvailable){
       return ( 
-        <CSSTransition classNames='order' key={key} timeout={{ enter: 250, exit: 250 }}>
+        <CSSTransition {...transitionOptions}>
           <li key={key}>
             Sorry {fish ? fish.name : 'fish'} is no longer available.
           </li>
@@ -21,10 +27,18 @@ export default class Order extends React.Component {
     }
 
     return (
-      <CSSTransition classNames='order' key={key} timeout={{enter: 250, exit: 250}}>
+      <CSSTransition {...transitionOptions}>
         <li key={key}>
-        {count} lbs {fish.name} {formatPrice(count * fish.price)}
-        <button onClick={() => this.props.removeFromOrder(key)}>&times;</button>
+          <span>
+            <TransitionGroup component="span" className="count">
+              <CSSTransition classNames="count" key={count} timeout={{ enter: 250, exit: 250 }}>
+                <span>{count}</span> 
+              </CSSTransition>
+            </TransitionGroup>
+            lbs {fish.name} 
+            {formatPrice(count * fish.price)}
+            <button onClick={() => this.props.removeFromOrder(key)}>&times;</button>
+          </span>
         </li>
       </CSSTransition>
     )
